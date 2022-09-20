067ea465b6e9a2eaf72bde6c2d683af05353ea01
Prince Churchill <princechurchill1000@gmail.com>
2022-09-20 02:00:52 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-   *main- print text
+   * main- print text
    *
    * Return: 0
    */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
