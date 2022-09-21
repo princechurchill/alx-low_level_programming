fb02bb641ff92f7bffc30a3df3d90b7dd8756c2f
Prince Churchill <princechurchill1000@gmail.com>
2022-09-21 09:00:22 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  * Return: 0
  */
 
-inbt main void
+int main void
 {
 	int n;
 
@@ -17,10 +17,10 @@ inbt main void
 
 	if (n > 0)
 		printf("%d is positive\n", n)
-	else if (n == 0)
-		printf("%d is zero\n", n)
-	else 
+	else if (n < 0)
 		printf("%d is negative\n", n)
+	else 
+		printf("%d is zero\n", n)
 
 	return (0);
 }
