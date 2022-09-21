58cc1079af0a8356bd616314c3c27c854518b185
Prince Churchill <princechurchill1000@gmail.com>
2022-09-21 08:03:33 -0700

    0-positive_or_negative.c

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints if number is positive, zero or negative
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	int n;
+
+	srand(time);
+	n=rand() - RAND_MAX / 2
+	
+
+	if (n<0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n==0)
+	{
+		printf("%d is zero/n", n);
+	}
+	else
+	{
+		print("%d is negative\n", n);
+	}
+	return (0);
+}
+
---
+++ 0x00-hello_world/testing.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * im trying my 
+ * hands
+ * on something
+ */
+
+int main()
+{
+	printf("%i\n ,300");
+	
+	
+	
+	return 0;
+
+
+}
