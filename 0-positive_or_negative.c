598d0366a332d64a8ad5afc144fa53932e0a9875
Prince Churchill <princechurchill1000@gmail.com>
2022-09-21 08:31:42 -0700

    0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -12,15 +12,15 @@ int main()
 {
 	int n;
 
-	srand(time);
+	srand((time));
 	n=rand() - RAND_MAX / 2
 	
 
-	if (n<0)
+	if (n < 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
 		printf("%d is zero/n", n);
 	}
@@ -28,6 +28,7 @@ int main()
 	{
 		print("%d is negative\n", n);
 	}
+
 	return (0);
 }
 
